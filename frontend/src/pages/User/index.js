4f575755987dff4625a3656f411b57314df5f0e0
Azril Nazli Alias <sales@stream.my>
2023-10-27 21:21:34 +0800

    User - fix tab to remove underline by using p-1 on Row

--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -9,8 +9,7 @@ import useUserStore from './stores/UserStore';
 import NewRegistration from './components/NewRegistration';
 
 const User = () => {
-    const store = useUserStore()
-
+ 
     const HandleTabChange = (key) => {
       //console.log(key)
 
@@ -19,30 +18,31 @@ const User = () => {
     }
 
     return (
-      <Tabs
-        defaultActiveKey="approve"
-        id="userTab"
-        className="mb-3"
-        onSelect={HandleTabChange}
-      >
-        <Tab eventKey="approve" title="Pendaftaran Baharu">
-          <NewRegistration role='user'/>
-        </Tab>
+      <Container className='p-1'>
+        <Tabs
+          defaultActiveKey="approve"
+          id="userTab"
+          className="mb-3"
+          onSelect={HandleTabChange}
+        >
+          <Tab eventKey="approve" title="Pendaftaran Baharu">
+            <NewRegistration role='user'/>
+          </Tab>
 
-        <Tab eventKey="user" title="Pengguna">
-          <UserTable role='user'/>
-        </Tab>
-        <Tab eventKey="admin" title="Admin">
-          <UserTable role='admin'/>
-        </Tab>
-        <Tab eventKey="approver-1" title="Pelulus 1">
-          <UserTable role='approver-1'/>
-        </Tab>
-        <Tab eventKey="approver-2" title="Pelulus 2">
-          <UserTable role='approver-2'/>
-        </Tab>  
-      </Tabs>
-
+          <Tab eventKey="user" title="Pengguna">
+            <UserTable role='user'/>
+          </Tab>
+          <Tab eventKey="admin" title="Admin">
+            <UserTable role='admin'/>
+          </Tab>
+          <Tab eventKey="manager" title="Manager">
+            <UserTable role='manager'/>
+          </Tab>
+          <Tab eventKey="boss" title="Boss">
+            <UserTable role='boss'/>
+          </Tab>  
+        </Tabs>
+      </Container>
     )
 } 
 
