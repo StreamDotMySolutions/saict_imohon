0c882c5deeff1691c20bd5d3c198a271047b2c2b
Azril Nazli Alias <sales@stream.my>
2023-11-13 07:59:52 +0800

    Inventory - index is working with Show Modal

--- frontend/src/pages/Item/index.js
+++ frontend/src/pages/Item/index.js
@@ -1,19 +1,23 @@
 import {useEffect, useState } from 'react'
 import axios from '../../libs/axios'
-import { Row,Col, Badge,Pagination } from 'react-bootstrap'
+import { Row,Col, Table, Badge,Pagination } from 'react-bootstrap'
 import ShowModal from './modals/ShowModal'
 import useInventoryStore from './stores/InventoryStore'
 
-const Application = () => {
+const Inventories = () => {
     const store = useInventoryStore()
     const [data, setData] = useState([])
-    const applications = data?.data?.inventories
+
  
     useEffect( () => {
         axios({url: store.url})
         .then( response => {
-          console.log(response)    
-          setData(response)  
+
+          setData({
+            'inventories': response.data.inventories.data,
+            'links': response.data.inventories.links
+          });
+
           useInventoryStore.setState({ refresh: false})
         })
 
@@ -24,82 +28,110 @@ const Application = () => {
     
       },[store.refresh,store.url])
 
+      function InventoryItems() {
+
+          const inventories = data.inventories
+
+          return (
+            <>
+              <thead>
+                  <tr>
+                      <th className='text-center'>ID</th>
+                      <th>VENDOR</th>
+                      <th className='text-center'>ITEM</th>
+                      <th className='text-center'>TOTAL</th>
+                      <th className='text-center'>DURATION</th>
+                      <th className='text-center'>RECEIVED ON</th>
+                      <th className='text-center'></th>
+                  </tr>
+              </thead>
+              <tbody>
+              {inventories?.map((item) => (
+                <tr key={item.id}>
+                  <td className='text-center'><Badge>{item.id}</Badge></td>
+                  <td>{item.vendor}</td>
+                  <td className='text-center'>{item.item}</td>
+                  <td className='text-center'>{item.total}</td>
+                  <td className='text-center'>From {item.date_start} to {item.date_end}</td>
+                  <td className='text-center'>{item.received_on}</td>
+                  <th className='text-center'>
+  
+                  </th>
+                </tr>
+              ))}
+              </tbody>
+            </>
+          );
+      }
+
+      function Paginator (){
+        const handlePaginationClick = (url) => {
+            useInventoryStore.setState({url: url})
+        }
+
+        const items =  data.links
+        const links = items?.map( (page,index) => 
+            
+            <Pagination.Item
+                key={index} 
+                active={page.active}
+                disabled={page.url === null}
+                onClick={() => handlePaginationClick(page.url)}
+                >
+                    <span dangerouslySetInnerHTML={{__html: page.label}} />
+            </Pagination.Item>
+        )
+    
+        return  (
+            <Pagination className='mt-3'>
+            {links}
+            </Pagination>
+        )
+    }
+
  
-
     return (
-        <div>
-            
-
-            <Row className='bg-light border border-1 rounded p-3'>
-                <Col className='col-1 fw-bold'>ID.</Col>
-                <Col className='fw-bold col-5'>Vendor</Col>
-
-
-            </Row>
-            <hr />
-            {applications?.data?.map((application,index) => (
-              <Row className='mb-2'>
-                <Col className='col-1'><Badge className='bg-dark'>{application.id}</Badge></Col>
-                <Col className='col-5'>{application.vendor}</Col>
-            
-                <Col className='text-center col-3'>
-                <ShowModal id={application.id} />
-      
-                </Col>
-            </Row>
+        <div className="col" >
+            <Table style={{ lineHeight: '2.5' }}>
+              <thead>
+                  <tr>
+                      <th className='text-center'>ID</th>
+                      <th>VENDOR</th>
+                      <th className='text-center'>ITEM</th>
+                      <th className='text-center'>TOTAL</th>
+                      <th className='text-center'>DURATION</th>
+                      <th className='text-center'>RECEIVED ON</th>
+                      <th className='text-center'></th>
+                  </tr>
+              </thead>
+              <tbody>
+              {data?.inventories?.map((item,index) => (
+              <tr key={item.id}>
+                <td className='text-center'><Badge>{item.id}</Badge></td>
+                <td>{item.vendor}</td>
+                <td className='text-center'>{item.item}</td>
+                <td className='text-center'>{item.total}</td>
+                <td className='text-center'>From {item.date_start} to {item.date_end}</td>
+                <td className='text-center'>{item.received_on}</td>
+                <td className='text-center'>
+                  <ShowModal id={item.id} />
+                </td>
+              </tr>
             ))}
+            </tbody>
+          </Table>
 
             
             <div className="d-flex bd-highlight mb-3">
                 <div className="ms-auto p-2 bd-highlight">
-                    <PaginatorLink items={applications} />
+                    <Paginator />
                 </div>
             </div>
+
         </div>
 
      
     );
 };
 
-function Item({label,value}){
-  return (<>
-
-    {value !== 0 &&
-      <button type="button" className="btn btn-sm btn-light border border-secondary mb-1">
-      {label} <span className="badge bg-secondary float-end">
-        {value}
-      </span>
-    </button>
-    }
-  </>)
-}
-/**
- * Paginator Links
- */
-function PaginatorLink ({items}){
-    //console.log(items.links)
-    const handlePaginationClick = (url) => {
-      //console.log(url)
-      useInventoryStore.setState({url: url})
-    }
-    const links = items?.links?.map( (page,index) => 
-        
-      <Pagination.Item
-          key={index} 
-          active={page.active}
-          disabled={page.url === null}
-          onClick={() => handlePaginationClick(page.url)}
-          >
-              <span dangerouslySetInnerHTML={{__html: page.label}} />
-      </Pagination.Item>
-    )
-  
-    return  (
-      <Pagination className='mt-3'>
-      {links}
-      </Pagination>
-    )
-  }
-  
-
-export default Application;
+export default Inventories;
