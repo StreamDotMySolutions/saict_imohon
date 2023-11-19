c4184323003db868dbb6fdbb8964b4f9319c5220
Azril Nazli Alias <sales@stream.my>
2023-11-19 13:14:59 +0800

    Distribution ~ Pagination Cache

--- backend/app/Services/DistributionService.php
+++ backend/app/Services/DistributionService.php
@@ -8,12 +8,20 @@ class DistributionService
 {
     public static function index()
     {
-        $paginate = Distribution::query()->with('application.user.userProfile.userDepartment');
-        $distributions = $paginate->orderBy('id','DESC')
-                                ->paginate(10)
-                                ->withQueryString();
-    
-        return $distributions;
+      
+        $page = \Request::input('page');
+        \Log::info($page);
+
+       $cached = \Cache::rememberForever('cached_distribution_index_' . $page , function () {
+            $paginate = Distribution::query()->with('application.user.userProfile.userDepartment');
+            $distributions = $paginate->orderBy('id','DESC')
+                                    ->paginate(10)
+                                    ->withQueryString();
+        
+            return $distributions;
+        });
+
+        return $cached;
     }
 
     public static function store(Request $request)
@@ -22,7 +30,8 @@ class DistributionService
         $distribution = new Distribution($request->except('acknowledge'));
         $distribution->user_id = $user->id;
         $distribution->save();
-        \Cache::forget('cached_distribution_' . $distribution->id); 
+        \Cache::flush(); 
+        //\Cache::forget('cached_distribution_' . $distribution->id); 
         return $distribution;
     }
 
@@ -33,7 +42,8 @@ class DistributionService
                             //->where('user_id', $user->id)
                             ->where('id',$distribution->id)
                             ->update($request->except('acknowledge','_method'));
-        \Cache::forget('cached_distribution_' . $distribution->id);    
+        \Cache::flush(); 
+        //\Cache::forget('cached_distribution_' . $distribution->id);    
         return $updated;                
     }
 
