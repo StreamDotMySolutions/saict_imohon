c6813c4dfe8cfbd67e804d62e068cad86d5df110
Azril Nazli Alias <sales@stream.my>
2023-12-21 19:28:35 +0800

    update cors

--- frontend/src/pages/Distribution/components/ApplicationInfo.js
+++ frontend/src/pages/Distribution/components/ApplicationInfo.js
@@ -53,6 +53,7 @@ const ApplicationInfo = () => {
                             <td>{data?.application?.created_at_formatted}</td>
                         </tr>
                     </Table>
+                    test
                 </Col>
             </>
             :
