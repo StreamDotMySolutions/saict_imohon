f0e68e68298d2787d053f5e7f555767b17be9d1f
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-28 08:29:21 +0800

    [AUTH] akses terhad

--- frontend/src/pages/Auth/components/Unauthorized/index.js
+++ frontend/src/pages/Auth/components/Unauthorized/index.js
@@ -7,7 +7,7 @@ const Unauthorized = () => {
     return (
     <Alert variant='warning'>
         <h1><FontAwesomeIcon icon="fa-solid fa-triangle-exclamation" /> {' '}Akses Terhad</h1>
-        Akaun anda tidak mempunyai akses ke sistem.
+        Akaun anda tidak mempunyai akses ke sistem. Admin belum mengesahkan lagi akaun anda. Harap maklum.
         <hr />
         <Link to='/sign-in'>
             <FontAwesomeIcon icon="fa-solid fa-reply" /> Laman utama
