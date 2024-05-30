15118c5fc3384fe16c4b50806237067e585ff030
StreamDotMy <streamdotmysolutions@gmail.com>
2024-05-30 19:20:03 +0800

    [FRONTEND] Admin update Mohon View

--- frontend/src/pages/MohonApproval/Admin/modals/ViewModal.js
+++ frontend/src/pages/MohonApproval/Admin/modals/ViewModal.js
@@ -32,8 +32,8 @@ export default function ViewModal({id}) {
       .then( response => {
           //console.log(response.data)
           let mohon = response.data.mohon
-          store.setValue('title', mohon.title) // set formValue
-          store.setValue('description', mohon.description) // set formValue
+          //store.setValue('title', mohon.title) // set formValue
+          //store.setValue('description', mohon.description) // set formValue
           setItems(mohon.mohon_items)
           setStep(mohon.mohon_approval.step)
 
@@ -117,7 +117,6 @@ export default function ViewModal({id}) {
           </Modal.Header>
 
           <Modal.Body>
-
             <MohonData id={id} />
           </Modal.Body>
           
