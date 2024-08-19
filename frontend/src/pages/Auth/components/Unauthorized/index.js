69bb5393ca6dae74756a6f79daef91111ecb6111
StreamDotMy <streamdotmysolutions@gmail.com>
2024-08-19 15:46:37 +0800

    [Logout] redirect to login

--- frontend/src/pages/Auth/components/Unauthorized/index.js
+++ frontend/src/pages/Auth/components/Unauthorized/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
 
 const Unauthorized = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Redirect to '/sign-in-by-nric' after 3 seconds (optional)
+        const timer = setTimeout(() => {
+            navigate('/sign-in-by-nric');
+        }, 1000);
+
+        // Cleanup the timer
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
+
     return (
     <Alert variant='warning'>
         <h1><FontAwesomeIcon icon="fa-solid fa-triangle-exclamation" /> {' '}Akses Terhad</h1>
