f4fa7749a864f38ea66ecac14fed2132fb6d0e8f
StreamDotMy <streamdotmysolutions@gmail.com>
2024-08-19 16:03:38 +0800

    [Footer] buang social media

--- frontend/src/layouts/components/UserLayout/Footer.js
+++ frontend/src/layouts/components/UserLayout/Footer.js
@@ -10,21 +10,7 @@ const Footer = () => {
     <footer className="py-3 bg-light">
       <div className="container">
       <div className="social-media-container">
-        <Link  to="https://www.tiktok.com/@beritartm?_t=8eNzEzhj1YV&_r=1">
-          <FontAwesomeIcon icon={faTiktok} className="social-media-icon tiktok-icon"/>
-        </Link>
-        <Link  to="https://www.facebook.com/BeritaRTM">
-          <FontAwesomeIcon icon={faFacebook} className="social-media-icon facebook-icon"/>
-        </Link>
-        <Link  to="https://www.youtube.com/channel/UCjUxj4cyy_F0tVCT8WfmdZQ" >
-          <FontAwesomeIcon icon={faYoutube} className="social-media-icon youtube-icon"/>
-        </Link>
-        <Link  to="https://twitter.com/beritartm?lang=en">
-          <FontAwesomeIcon icon={faTwitter} className="social-media-icon twitter-icon"/>
-        </Link>
-        <Link  to="https://www.instagram.com/beritartm/?hl=en">
-          <FontAwesomeIcon icon={faInstagram} className="social-media-icon instagram-icon"/>
-        </Link>
+        
       </div>
       <div className="row mt-3">
         <Col md={4}>
