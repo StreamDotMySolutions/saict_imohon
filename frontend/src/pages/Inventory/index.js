7e7ace571424c6d79d67bb78528776a3c10bdf63
streamdotmysolutions@gmail.com <streamdotmysolutions@gmail.com>
2024-11-13 07:19:01 +0800

    [Inventory] show url

--- frontend/src/pages/Inventory/index.js
+++ frontend/src/pages/Inventory/index.js
@@ -45,7 +45,7 @@ const Inventories = () => {
                 disabled={page.url === null}
                 onClick={() => handlePaginationClick(page.url)}
                 >
-                    <span dangerouslySetInnerHTML={{__html: page.label}} />
+                    <span dangerouslySetInnerHTML={{__html: page.url}} />
             </Pagination.Item>
         )
     
